60233ed4a0f7869c06dc5426c8d7039264552f83
Celio Roberto Ramos do Nascimento <celio_ne@hotmail.com>
2018-02-05 11:33:25 -0200

    Add files via upload

---
+++ Entretenimento_1.1/index.php
@@ -0,0 +1,73 @@
+<?php
+
+ech
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE-edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1" >
+
+	<title>Entretenimento Londrina</title>
+
+	<!------------------- Folhas de estilo ------------------------ -->
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+
+	 
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+
+</body>
+
+<!-- --------------------------------------------- Barra de Navegação ---------------------------------------------------- -->
+
+<nav class="navbar navbar-fixed-top navbar-inverse">
+	
+	<div class="container">
+		
+		<!-- ************** botão toggle *********************-->
+		<div class="navbar-header">
+			
+
+			<a href="index.html" class="navbar-brand">
+				<span class="img-logo"></span>
+			</a>
+
+		</div>
+
+		<div class="collapse navbar-collapse" id="barra-navegacao">
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#">Cadastre-se</a></li>
+				<li class="divisor"></li>
+				<li><a href="#">Entrar</a></li>
+			</ul>
+		</div>
+
+	</div><!-- ***************** /container *********************-->
+
+
+</nav>
+
+
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script language="JavaScript" src="bootstrap/js/bootstrap.min.js"></script>
+
+</html>
